807de628710088de6770d894cd4a080a6e706457
dariodf <dario@getsavi.com>
2025-08-29 16:02:44 -0600

    clean output

--- lib/tasks/compile.remote.ex
+++ lib/tasks/compile.remote.ex
@@ -5,28 +5,31 @@ defmodule Mix.Tasks.Compile.Remote do
 
   @impl true
   def run(args) do
+    {opts, _args, _err} = OptionParser.parse(args, switches: [verbose: :boolean], aliases: [v: :verbose])
+    verbose = opts[:verbose] || false
+
     case args do
       ["in_remote"] ->
         Mix.shell().info("Running remote compilation (in_remote)")
         {:ok, []}
 
       _ ->
-        remote_compile()
+        remote_compile(verbose)
     end
   end
 
-  defp remote_compile do
+  defp remote_compile(verbose) do
     remote = System.fetch_env!("REMOTE_HOST")
     remote_path = System.fetch_env!("REMOTE_PATH")
     build_path = Mix.Project.build_path()
     env = Mix.env()
+    rsync_flags = if verbose, do: "--progress", else: "--quiet"
 
     # 1. Sync project skeleton (no deps/_build)
-    run_cmd("""
-    rsync -azh --delete --progress ./ #{remote}:#{remote_path}/
-    """)
+    IO.puts("Syncing project skeleton...")
+    run_cmd("rsync -azh --delete #{rsync_flags} ./ #{remote}:#{remote_path}/", verbose)
 
-    # 2. Detect remote mix path inline
+    # 2. Detect remote mix path
     {remote_mix_path, status} =
       System.cmd(
         "ssh",
@@ -49,7 +52,8 @@ defmodule Mix.Tasks.Compile.Remote do
       exit({:shutdown, 1})
     end
 
-    # 3. Ensure remote deps folder exists and run mix deps.get remotely
+    # 3. Run remote deps.get
+    IO.puts("Syncing deps on remote...")
     remote_deps_cmd = """
     ssh #{remote} 'bash -lc "\
     source ~/.bashrc >/dev/null 2>&1; \
@@ -59,17 +63,30 @@ defmodule Mix.Tasks.Compile.Remote do
     cd #{remote_path} && \
     MIX_ENV=#{env} #{remote_mix_path} deps.get in_remote"'
     """
-    run_stream_cmd(remote_deps_cmd)
+    run_stream_cmd(remote_deps_cmd, verbose)
 
     # 4. Sync deps back
-    run_cmd("ssh #{remote} 'mkdir -p #{remote_path}/deps'")
-    run_stream_cmd("""
-    rsync -azh --delete --times --modify-window=10 --progress \
-      #{remote}:#{remote_path}/deps/ ./deps/
-    """)
+    run_cmd("ssh #{remote} 'mkdir -p #{remote_path}/deps'", verbose)
+    IO.puts("Syncing deps back locally...")
+    run_stream_cmd("rsync -azh --delete --times --modify-window=10 #{rsync_flags} #{remote}:#{remote_path}/deps/ ./deps/", verbose)
 
-    # 5. Ensure remote _build exists and run remote compilation
-    remote_compile_cmd = """
+    # 5. Run remote compilation (always show remote compile output, hide SSH command line)
+    IO.puts("Running remote compilation...")
+    run_stream_cmd(remote_compile_cmd(remote, remote_path, env, remote_mix_path), false)
+
+    # 6. Sync build artifacts back
+    IO.puts("Syncing build artifacts...")
+    run_stream_cmd("rsync -azh --delete --times --modify-window=10 #{rsync_flags} #{remote}:#{remote_path}/_build/ ./_build/", verbose)
+
+    # 7. Touch manifests locally
+    manifests = Path.wildcard(Path.join(build_path, "**/.mix/compile.*"))
+    Enum.each(manifests, &File.touch!(&1, :os.system_time(:second)))
+
+    {:ok, []}
+  end
+
+  defp remote_compile_cmd(remote, remote_path, env, remote_mix_path) do
+    """
     ssh #{remote} 'bash -lc "\
     source ~/.bashrc >/dev/null 2>&1; \
     source ~/.profile >/dev/null 2>&1; \
@@ -78,26 +95,11 @@ defmodule Mix.Tasks.Compile.Remote do
     cd #{remote_path} && \
     MIX_ENV=#{env} #{remote_mix_path} compile in_remote"'
     """
-    run_stream_cmd(remote_compile_cmd)
-
-    # 6. Sync build artifacts back
-    run_stream_cmd("""
-    rsync -azh --delete --times --modify-window=10 --progress \
-      #{remote}:#{remote_path}/_build/ ./_build/
-    """)
-
-    # 7. Touch manifests locally
-    manifests = Path.wildcard(Path.join(build_path, "**/.mix/compile.*"))
-    Enum.each(manifests, fn file ->
-      File.touch!(file, :os.system_time(:second))
-    end)
-
-    {:ok, []}
   end
 
   # Run a command and wait for it
-  defp run_cmd(cmd) do
-    IO.puts("Running: #{cmd}")
+  defp run_cmd(cmd, verbose) do
+    if verbose, do: IO.puts("Running: #{cmd}")
 
     case System.cmd("sh", ["-c", cmd], stderr_to_stdout: true) do
       {_, 0} -> :ok
@@ -108,8 +110,8 @@ defmodule Mix.Tasks.Compile.Remote do
   end
 
   # Run a command and stream output live
-  defp run_stream_cmd(cmd) do
-    IO.puts("Running (streaming): #{cmd}")
+  defp run_stream_cmd(cmd, verbose) do
+    if verbose, do: IO.puts("Running (streaming): #{cmd}")
     port = Port.open({:spawn, cmd}, [:binary, :exit_status, :stderr_to_stdout])
     stream(port)
   end
